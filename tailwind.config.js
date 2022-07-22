85f75fa4a0f0719172b749f911e3938a6573a185
Laeeq <laeeqhaq@gmail.com>
2022-07-22 19:12:31 +0600

    automated commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,17 @@
 module.exports = {
   content: [
     "./**.html",
-    "./pages/**{.tsx,jsx,ts,js}",
-    "./components/**{.tsx,jsx,ts,js}",
+    "./pages/**.{tsx,jsx,ts,js}",
+    "./components/**.{tsx,jsx,ts,js}",
   ],
   theme: {
     extend: {
       screens: {
         mdlg: "1280px",
         xlg: "1600px",
+      },
+      colors: {
+        dodgerBlue: "#0070f3",
       },
     },
   },
