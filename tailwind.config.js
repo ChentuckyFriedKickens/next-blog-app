ce9ff9666fe5ce9d2d39c2e22fa236e3ea2071a9
ChentuckyFriedKickens <70830912+ChentuckyFriedKickens@users.noreply.github.com>
2022-07-22 18:42:50 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./**.html",
+    "./pages/**{.tsx,jsx,ts,js}",
+    "./components/**{.tsx,jsx,ts,js}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        mdlg: "1280px",
+        xlg: "1600px",
+      },
+    },
+  },
+  plugins: [],
+};
